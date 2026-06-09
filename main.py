badadf1e9d01abee1c335a6847c5c09114969609
ProgrammerBro1 <167418178+ProgrammerBro1@users.noreply.github.com>
2026-06-10 00:56:10 +0300

    Commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.include_router(api_router, prefix="/api")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's dev server address
+    allow_origins=["http://localhost:5173","https://riri-frontend.vercel.app"],  # Vite's dev server address
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
